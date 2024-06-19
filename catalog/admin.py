6abfa7e69fb6e815094dead11c29c8cd5471a996
Churilov Evgeny <i@churilovevgeny.ru>
2024-06-19 23:10:39 +0300

    Добавлена админка, убран из модели лишний столбец

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "price_per_purchase", "category")
+
+
+@admin.register(Category)
+class CateoryAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name")
