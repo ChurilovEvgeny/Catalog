5edb2596093bb80a5d1281fd56d832bfaa0e0ee2
Churilov Evgeny <i@churilovevgeny.ru>
2024-07-31 13:57:12 +0300

    Реализовано ограничение в редактировании блогов

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from smtplib import SMTPAuthenticationError
-
-from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy, reverse
@@ -8,7 +8,7 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 from pytils.translit import slugify
 
 from blog.models import Blog
-from config.settings import EMAIL_HOST_USER
+from utils.utils import send_mail_async
 
 
 class BlogCreateView(CreateView):
@@ -37,46 +37,42 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
 
-    @staticmethod
-    def send_notification(title):
-        try:
-            send_mail(
-                'Сообщение с сайта',
-                f'Ваш блог {title} достиг 100 просмотров',
-                EMAIL_HOST_USER,
-                ["churilov.e.a@yandex.ru"],
-                fail_silently=False,
-            )
-        except SMTPAuthenticationError:
-            print('Не удалось авторизоваться на почте')
-        except Exception as e:
-            print("Ошибка отправки сообщения' + e")
-
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
         self.object.views_count += 1
 
         # Если количество просмотров достигло 100, то отправляем сообщение на почту
         if self.object.views_count == 100:
-            self.send_notification(self.object.title)
+            send_mail_async('Сообщение с сайта', f'Ваш блог {self.object.title} достиг 100 просмотров',
+                            ["churilov.e.a@yandex.ru"], )
 
         self.object.save()
         return self.object
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Blog
     fields = ('title', 'slug', 'body', 'preview', 'is_published')
+    login_url = reverse_lazy('users:login')
 
     def get_success_url(self):
         return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
+    def get_form_class(self):
+        user = self.request.user
+        if user.is_superuser or user.has_perm("blog.can_control_blog"):
+            return super().get_form_class()
 
-class BlogDeleteView(DeleteView):
+        raise PermissionDenied
+
+
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Blog
     success_url = reverse_lazy('blog:blog_list')
+    login_url = reverse_lazy('users:login')
 
 
+@login_required(login_url=reverse_lazy('users:login'))
 def blog_toggle_is_published(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     blog.is_published = not blog.is_published
