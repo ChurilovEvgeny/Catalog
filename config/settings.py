662e4edbdda751a7ce30017361b4a38408f07f19
Churilov Evgeny <i@churilovevgeny.ru>
2024-08-05 16:55:46 +0300

    Добавлен Redis в проект

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,12 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379'
+        }
+    }
